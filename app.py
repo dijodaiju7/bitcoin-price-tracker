996cc2478c247fcc632a195b904f7a30c3216b3c
Dijo Daiju <60286610+dijodaiju7@users.noreply.github.com>
2020-11-13 09:37:04 +0530

    Updated Files

--- app.py
+++ app.py
@@ -7,13 +7,25 @@ model = pickle.load(open('prediction.pickle', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('predict.html')
+    return render_template('login.html')
+database={'admin':'123','test':'123','demo':'123'}
 
-@app.route('/index',methods=['GET','POST'])
+@app.route('/login',methods=['POST','GET'])
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+	         return render_template('predict.html',name=name1)
+
+
+@app.route('/home',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
-
-
+    return render_template("index.html")
 
 
 @app.route('/predict',methods=['GET','POST'])
